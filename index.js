77f47c7e51e6d6d1dc8cd607715652e0c460576e
github username <github email address>
2020-07-07 00:33:42 -0400

    Done.

--- index.js
+++ index.js
@@ -9,13 +9,22 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  let leftNumbers = dodger.style.right.replace("px", "");
+  let leftNumbers = dodger.style.left.replace("px", "");
   let left = parseInt(leftNumbers, 10);
   if(left > 0) {
       dodger.style.left = `${left + 1}px`;
   }
 }
 
-document.addEventListener("keydown", function(e) {
-  console.log(e.key);
+document.addEventListener('keydown', function(e) {
+  //console.log(e.key);
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft()
+  }
+});
+
+document.addEventListener('keydown', function(e) {
+  if (e.key === "ArrowRight") {
+    moveDodgerRight()
+  }
 });
